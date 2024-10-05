4a62e5d870f0cf4a8acf431d3e802c56a0e2bb4c
Your Name <alaaahmedsaber2002@gmail.com>
2024-10-06 02:52:36 +0300

    restyle: shop page to be responsive

--- my-app/src/app/(user)/shop/page.tsx
+++ my-app/src/app/(user)/shop/page.tsx
@@ -8,6 +8,7 @@ import { ProductProps } from "@/type"
 import { useEffect, useState } from "react"
 import { BsGridFill } from "react-icons/bs"
 import { ImList } from "react-icons/im"
+import useMediaQuery from '@mui/material/useMediaQuery';
 
 const ShopPage = () => {
 
@@ -16,6 +17,7 @@ const ShopPage = () => {
     const [productData, setProductData] = useState([]);
     const [visibleProducts, setVisibleProducts] = useState(8)
     const productsNum = productData.length
+    const smallScreen = useMediaQuery('(min-width:500px)')
 
     const handleSeeMore = ()=>{
         setVisibleProducts(visibleProducts + 8)
@@ -37,49 +39,51 @@ const ShopPage = () => {
         <Container>
             <div className="flex items-center justify-between pb-10">
                 <h2 className="text-2xl text-primeColor font-bold">All Products</h2>
-                <div className="flex items-center gap-4">
-                    <span
-                        onClick={() => {
-                            setShowGrid(true);
-                            setShowList(false);
-                            setVisibleProducts(8)
-                        }}
-                        className={`${
-                            showGrid
-                                ? "bg-primeColor text-white"
-                                : "border-[1px] border-gray-300 text-[#737373]"
-                            } w-8 h-8 text-lg flex items-center justify-center cursor-pointer`
-                        }
-                    >
-                        <BsGridFill />
-                    </span>
-                    <span
-                        onClick={() => {
-                            setShowGrid(false);
-                            setShowList(true);
-                            setVisibleProducts(8)
-                        }}
-                        className={`${
-                            showList
-                                ? "bg-primeColor text-white"
-                                : "border-[1px] border-gray-300 text-[#737373]"
-                            } w-8 h-8 text-base flex items-center justify-center cursor-pointer listView`
-                        }
-                    >
-                        <ImList />
-                    </span>
-                </div>
-            </div>
-            {showGrid ? (
-                <div className="w-full grid grid-cols-1 md:grid-cols-2 xl:grid-cols-4 gap-5">
-                    {productData?.slice(0, visibleProducts).map((item: ProductProps) => (
-                        <ProductCard key={item?._id} product={item} />
-                    ))}
+                {smallScreen &&
+                    <div className="flex items-center gap-4">
+                        <span
+                            onClick={() => {
+                                setShowGrid(true);
+                                setShowList(false);
+                                setVisibleProducts(8)
+                            }}
+                            className={`${
+                                showGrid
+                                    ? "bg-primeColor text-white"
+                                    : "border-[1px] border-gray-300 text-[#737373]"
+                                } w-8 h-8 text-lg flex items-center justify-center cursor-pointer`
+                            }
+                        >
+                            <BsGridFill />
+                        </span>
+                        <span
+                            onClick={() => {
+                                setShowGrid(false);
+                                setShowList(true);
+                                setVisibleProducts(8)
+                            }}
+                            className={`${
+                                showList
+                                    ? "bg-primeColor text-white"
+                                    : "border-[1px] border-gray-300 text-[#737373]"
+                                } w-8 h-8 text-base flex items-center justify-center cursor-pointer listView`
+                            }
+                        >
+                            <ImList />
+                        </span>
                     </div>
-            ) : (
+                }
+            </div>
+            {showList && smallScreen ? (
                 <div className="w-full grid grid-cols-1 gap-5">
                     {productData?.slice(0, visibleProducts).map((item: ProductProps) => (
                         <ListProduct key={item?._id} product={item} />
+                    ))}
+                </div>
+            ) : (
+                <div className="w-full grid grid-cols-1 md:grid-cols-2 xl:grid-cols-4 gap-5">
+                    {productData?.slice(0, visibleProducts).map((item: ProductProps) => (
+                        <ProductCard key={item?._id} product={item} />
                     ))}
                 </div>
             )}
